b84a95f8442529bce7f063a81ee7779b4befeeea
araz <araz944memmedov@gmail.com>
2025-02-12 20:33:19 +0400

    Changes in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,18 @@ const mainTailwindConfig = require("../main-mf/tailwind.config.js");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   ...mainTailwindConfig,
+  theme: {
+    extend: {
+      ...mainTailwindConfig.theme?.extend,
+      width: {
+        ...mainTailwindConfig.theme?.extend?.width,
+        '23vw': '23vw',
+        '25vw': '25vw',
+      },
+      colors: {
+        ...mainTailwindConfig.theme?.extend?.colors,
+        'black-99': '#00000099',
+      },
+    }
+  }
 }
