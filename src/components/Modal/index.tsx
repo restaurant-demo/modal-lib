bde3280ae8229449ad158d8b59112f94d1a83478
araz <araz944memmedov@gmail.com>
2025-02-14 13:42:43 +0400

    Add conditional rendering for modal types

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import "./Modal.css";
-import MenuModal from "./MenuModal";
+import React from 'react';
+import './Modal.css';
+import MenuModal from './MenuModal';
+import TablesModal from './TablesModal';
+import { EModalType } from "main/modalTypes";
+import { useModalStore } from 'main/modalStore';
 
 const Modal = () => {
+  const { modal } = useModalStore();
+
   return (
     <div className="custom-modal-layout">
-      <MenuModal />
+      {modal.isOpen && modal.type === EModalType?.MENU && <MenuModal />}
+      {modal.isOpen && modal.type === EModalType?.TABLES && <TablesModal />}
     </div>
   );
 };
