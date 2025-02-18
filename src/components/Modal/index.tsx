d5206bf88aeeb2eb5af8d5223ea451399ff195e7
araz <araz944memmedov@gmail.com>
2025-02-18 18:38:30 +0400

    Add state for Cabinet modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import './Modal.css';
 import MenuModal from './MenuModal';
 import TablesModal from './TablesModal';
-import { EModalType } from "main/modalTypes";
+import { EModalType } from 'main/modalTypes';
 import { useModalStore } from 'main/modalStore';
+import CabinetsModal from './CabinetsModal';
 
 const Modal = () => {
   const { modal } = useModalStore();
 
   return (
     <div className="custom-modal-layout">
-      {modal.isOpen && modal.type === EModalType?.MENU && <MenuModal />}
-      {modal.isOpen && modal.type === EModalType?.TABLES && <TablesModal />}
+      <section className="custom-modal-section">
+        <h2 className="custom-modal-title">Add new {modal.type}</h2>
+        {modal.isOpen && modal.type === EModalType?.MENU && <MenuModal />}
+        {modal.isOpen && modal.type === EModalType?.TABLE && <TablesModal />}
+        {modal.isOpen && modal.type === EModalType?.CABINET && <CabinetsModal />}
+      </section>
     </div>
   );
 };
